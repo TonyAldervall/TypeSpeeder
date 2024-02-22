4140d8c34de330be4b222c8579df8775e10dd9c2
Tony Aldervall <tony.aldervall@iths.se>
2024-02-22 14:43:54 +0100

    small fixes for test classes

--- src/test/java/se/ju23/typespeeder/ChallengeTest.java
+++ src/test/java/se/ju23/typespeeder/ChallengeTest.java
@@ -2,6 +2,7 @@ package se.ju23.typespeeder;
 import org.junit.jupiter.api.Test;
 
 import java.lang.reflect.Method;
+import java.util.Scanner;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -28,7 +29,7 @@ public class ChallengeTest {
     public void testStartChallengeMethodExists() {
         try {
             Class<?> challengeClass = Class.forName("se.ju23.typespeeder.Challenge");
-            Method method = challengeClass.getMethod("startChallenge", String.class);
+            Method method = challengeClass.getMethod("startChallenge", String.class, Scanner.class, Account.class);
             assertNotNull(method, "The method 'startChallenge' should exist in the Challenge class.");
         } catch (NoSuchMethodException | ClassNotFoundException e) {
             fail("The method 'startChallenge' could not be found in the Challenge class.");
